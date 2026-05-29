ff0850d9eecc2d72f0968ad284f39af1c7869288
Jesus Miguel Adrian Matos <elbuho12@outlook.com>
2026-05-29 21:03:44 +0200

    change train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,15 @@
 python train.py -s /home/jesus/Documents/Gaussian_splatting/360_extra_scenes/bonsai \
-    -m output/m360/bonsai_beta_run3 \
+    -m output/m360/bonsai_beta_run4 \
     --iterations 50000 \
     --test_iterations 7000 15000 30000 50000 \
     --densify_until_iter 45000 \
-    --lambda_normal 0.0 \
-    --lambda_dist 0.0 \
+    --lambda_normal 0.05 \
+    --lambda_dist 100 \
+    --opacity_reset_interval 3000 \
     --cap_max 2500000 \
     --noise_lr 5e4 \
     --scale_reg 0.005 \
     --opacity_cull 0.005 \
     --mcmc_error_weight 2.0 \
     --mcmc_jitter_scale 1.5 \
-    2>&1 | tee logs/bonsai3.log
+    2>&1 | tee logs/bonsai4.log
