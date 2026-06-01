71c9375dc9ed32c7902bcf2a008fac4eb063c0f6
Jesus Miguel Adrian Matos <elbuho12@outlook.com>
2026-06-01 14:07:48 +0200

    change train.sh

---
+++ train_server.sh
@@ -0,0 +1,16 @@
+python train.py -s Datasets/bonsai \
+    -m output/m360/bonsai_beta_run4 \
+    --iterations 50000 \
+    --test_iterations 7000 15000 30000 50000 \
+    --densify_until_iter 45000 \
+    --lambda_normal 0.05 \
+    --lambda_dist 10 \
+    --opacity_reset_interval 3000 \
+    --cap_max 1500000 \
+    --noise_lr 5e4 \
+    --scale_reg 0.005 \
+    --opacity_cull 0.005 \
+    --mcmc_error_weight 2.0 \
+    --mcmc_jitter_scale 1.5 \
+    2>&1 | tee logs/bonsai4.log
+
