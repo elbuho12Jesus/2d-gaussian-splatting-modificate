09451a839ecda7fb57a96e6e570f5cc1175a17ef
Jesus Miguel Adrian Matos <elbuho12@outlook.com>
2026-06-04 02:23:54 +0200

    train.sh update

--- train_server.sh
+++ train_server.sh
@@ -1,18 +1,19 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-python train.py -s Datasets/bonsai \
-    -m output/m360/bonsai_beta_run4 \
+python train.py -s Datasets/flowers \
+    -m output/m360/flowers_beta_run2 \
     --iterations 50000 \
     --test_iterations 7000 15000 30000 50000 \
     --densify_until_iter 45000 \
     --lambda_normal 0.05 \
-    --lambda_dist 10 \
-    --opacity_reset_interval 3000 \
-    --cap_max 1500000 \
-    --noise_lr 5e4 \
+    --lambda_dist 40 \
+    --opacity_reset_interval 1000000000 \
+    --cap_max 6000000 \
+    --noise_lr 5e2 \
     --scale_reg 0.005 \
     --opacity_cull 0.005 \
     --mcmc_error_weight 2.0 \
     --mcmc_jitter_scale 1.5 \
-    2>&1 | tee logs/bonsai4.log
-
+    --cov_noise \
+    --cov_noise_normal 1.0 \
+    2>&1 | tee logs/flowers2.log
